e03b227e557594d140659587b9e4a36e6fa786af
riccardo_pizzi <riccardo_pizzi@mckinsey.com>
2020-02-13 21:49:07 +0100

    Cleaned code

--- src/data/helpers.py
+++ src/data/helpers.py
@@ -1,6 +1,7 @@
 from src.api.fetch import NBAFetcher
 from src.utils.config import Config
 
+
 def get_nba_fetcher():
     auth_config = Config.get_auth_config()
     api_key = auth_config["api_key"]
--- src/data/load_data.py
+++ src/data/load_data.py
@@ -1,64 +1,66 @@
-###################################################################################################
-# Loading games and stats
-###################################################################################################
-
-from src.data.helpers import get_nba_fetcher
 import pandas as pd
-import os
-import json
-from pathlib import Path
 
-data_path = Path("/Users/riccardo_pizzi/Documents/project_data/ninja")
-game_ids_path = Path("/Users/riccardo_pizzi/Documents/project_data/ninja/game_stats")
-game_stats_path = Path("/Users/riccardo_pizzi/Documents/project_data/ninja/game_stats")
+from src.data import checks
+from src.data.helpers import get_nba_fetcher
+from src.utils.logging import MyLogger
+from src.utils import paths, json_utils
+
+logger = MyLogger()
 
 
-def get_game_stats_year(year, game_ids_path, game_stats_path, data_path):
-    check = (game_ids_path / f'{str(year)}.json').exists()
-
-    if check:
-        game_id_path = game_ids_path / f'{str(year)}.json'
-        with open(game_id_path, encoding='utf-8') as f:
-            data = json.load(f)
-        games = pd.DataFrame(data)
-    else:
-        print(f'Pulling directly from API')
-        nba_fetcher = get_nba_fetcher()
-        games = nba_fetcher.get_games_by_season_year(year=year)
-
-        if not os.path.exists(game_ids_path):
-            os.mkdir(game_ids_path)
-
-        file_name = f'{year}.json'
-        games.to_json(game_ids_path/file_name)
-
+def load_all_game_stats_in_year(year):
+    games = load_games(year)
     game_stats_list = []
-
-    for game_id in games['gameId']:
-
-        check = (game_stats_path/f'{str(game_id)}.json').exists()
-
-        if check:
-            game_id_path = game_stats_path/f'{str(year)}.json'
-            with open(game_id_path, encoding='utf-8') as f:
-                data = json.load(f)
-            game_stats = pd.DataFrame(data)
-        else:
-            print(f'Pulling directly from API')
-            nba_fetcher = get_nba_fetcher()
-            game_stats = nba_fetcher.get_game_stats(game_id)
-            game_stats_path = data_path / 'game_stats'
-
-            if not os.path.exists(game_stats_path):
-                    os.mkdir(game_stats_path)
-
-            game_stats_list.append(game_stats)
-
-        file_name = f'{game_id}.json'
-        with open(file_name, "w") as fp:
-            json.dump(game_stats_list, fp)
-
-        return game_stats_list
+    for game_id in games["gameId"]:
+        game_stats_list.append(load_game_stats(game_id))
+    return game_stats_list
 
 
-get_game_stats_year(2018, game_ids_path, game_stats_path, data_path)
+def load_games(year: int):
+    if checks.check_game_ids_exist_locally(year):
+        return load_games_from_local(year)
+    else:
+        logger.info(
+            f"Game IDs for year {str(year)} are not saved locally. Pulling directly from API"
+        )
+        return load_games_from_api(year)
+
+
+def load_game_stats(game_id: int):
+    if checks.check_game_stats_exist_locally(game_id):
+        return load_game_stats_from_local(game_id)
+    else:
+        logger.info(
+            f"Game stats for game_id {str(game_id)} are not saved locally. Pulling directly from "
+            f"API"
+        )
+        return load_game_stats_from_api(game_id)
+
+
+def load_games_from_local(year: int):
+    game_ids_path = paths.get_game_ids_path()
+    games = pd.DataFrame(json_utils.read_json(game_ids_path / f"{str(year)}.json"))
+    return games
+
+
+def load_games_from_api(year: int):
+    nba_fetcher = get_nba_fetcher()
+    games = nba_fetcher.get_games_by_season_year(year=year)
+    game_ids_path = paths.get_game_ids_path()
+    file_name = f"{year}.json"
+    games.to_json(game_ids_path / file_name)
+    return games
+
+
+def load_game_stats_from_local(game_id):
+    game_stats_path = paths.get_game_stats_path()
+    return json_utils.read_json(game_stats_path / f"{str(game_id)}.json")
+
+
+def load_game_stats_from_api(game_id: int):
+    nba_fetcher = get_nba_fetcher()
+    game_stats = nba_fetcher.get_game_stats(game_id)
+    game_stats_path = paths.get_game_stats_path()
+    file_name = f"{game_id}.json"
+    json_utils.save_json(game_stats, game_stats_path / file_name)
+    return game_stats
